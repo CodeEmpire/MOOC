1f240da31ac989612c45461e6a186b017c383594
Marcin Rozycki <rozyckimarcin@wp.pl>
2016-12-05 23:04:53 +0100

    excercise 5

--- src/pl/rozycki/marcin/Lesson1.java
+++ src/pl/rozycki/marcin/Lesson1.java
@@ -107,6 +107,9 @@ public class Lesson1 {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     /* YOUR CODE HERE */
+    Runnable task = () -> System.out.println(list);
+
+    task.run();
   }
 
   /**
